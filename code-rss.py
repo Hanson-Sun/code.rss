517fd9ed74a8340c65ffcb7dd98a53d1bfb8ab8e
Alx-Li <li.alex.0901@gmail.com>
2020-12-24 16:10:47 -0800

    link contact us

--- code-rss.py
+++ code-rss.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
